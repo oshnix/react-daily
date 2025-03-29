d4f6d05f955cf63b5200efe29ee64ed31814e76d
Polina_Sloboda <sloboda.polina@gmail.com>
2025-03-29 19:14:36 +0400

    RD-3 - add the mantine library, update theme, add task form

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { App } from './App.tsx';
+import '@mantine/core/styles.css';
+import '@mantine/dates/styles.css';
 import '../styles/index.scss';
-import { App } from './app/App.tsx';
 
 createRoot(document.getElementsByTagName('body').item(0)!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <>
+    <link
+      href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet"
+    />
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </>,
 );
